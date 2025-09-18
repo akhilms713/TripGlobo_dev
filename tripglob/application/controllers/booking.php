9cf418a5d65b0028f0ef8356eb7d2ac2a6fb4606
Devaraj <devarajpd822@gmail.com>
2025-09-18 21:25:56 +0530

    flight data changed

--- tripglob/application/controllers/booking.php
+++ tripglob/application/controllers/booking.php
@@ -535,7 +535,7 @@ public function booking_travellers_conn($secdata,$pcode,$pcode_d,$pcode_f,$searc
 			);
 				$booking_xml_data_id = $this->booking_model->insert_booking_xml_data($booking_xml);
 				$booking_supplier = array(
-				'api_id' =>$cart_hotel_data->api_id,
+				'api_id' =>$cart_flight_data->api_id,
 					'booking_supplier_number' => 'XXXXXXXXXX',
 					'supplier_status' => '',
 					'supplier_reference_number' => '',
@@ -906,7 +906,7 @@ public function booking_travellers_conn($secdata,$pcode,$pcode_d,$pcode_f,$searc
 				);
 				$booking_xml_data_id = $this->booking_model->insert_booking_xml_data($booking_xml);
 				$booking_supplier = array(
-					'api_id' =>$cart_hotel_data->api_id,
+					'api_id' =>$cart_flight_data->api_id,
 					'booking_supplier_number' => 'XXXXXXXXXX',
 					'supplier_status' => '',
 					'supplier_reference_number' => '',
@@ -1183,7 +1183,7 @@ public function booking_travellers_conn($secdata,$pcode,$pcode_d,$pcode_f,$searc
 					$booking_xml_data_id = $this->booking_model->insert_booking_xml_data($booking_xml);
 					*/
 					$booking_supplier = array(
-						'api_id' =>$cart_hotel_data->api_id,
+						'api_id' =>$cart_flight_data->api_id,
 						'booking_supplier_number' => 'XXXXXXXXXX',
 						'supplier_status' => '',
 						'supplier_reference_number' => '',
