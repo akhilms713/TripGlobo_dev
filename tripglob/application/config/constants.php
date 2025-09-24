db0b7a7523fa24c760b3c33e92471d655602b7da
Ganga B S <96790034+GangaBS99@users.noreply.github.com>
2025-09-24 19:19:49 +0530

    Update constants.php
    reverting to old code base url

--- tripglob/application/config/constants.php
+++ tripglob/application/config/constants.php
@@ -37,7 +37,7 @@ if ($env_base_url && strlen(trim($env_base_url)) > 0) {
 
     $base_url = $protocol . $host . $path;
 } else {
-    $base_url = "https://dev-tripglobo.dsocmarket.com/";  // no trailing slash
+    $base_url = "http://localhost/tripglobo_dev/tripglob";  // no trailing slash
 }
 
 /*
